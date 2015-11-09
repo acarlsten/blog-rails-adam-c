54239ed1a2dc5326bf53383ef752afa95eea908c
Adam Carlsten <addecrea3@hotmail.com>
2015-11-09 20:00:29 +0100

    blaaa

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,9 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+
+    @comment = Comment.new
+    @comment.article_id = @article.id
   end
 
   def new
@@ -38,7 +41,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-    
+
     flash.notice = "Article '#{@article.title}' Deleted!"
 
     redirect_to articles_path
